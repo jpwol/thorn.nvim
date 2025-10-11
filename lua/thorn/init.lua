7731f12b8016feedc8380bac1f2a010528193130
jpwol <jpwolfe1981@gmail.com>
2025-10-11 19:03:51 -0400

    updated readme

--- lua/thorn/init.lua
+++ lua/thorn/init.lua
@@ -1,25 +1,25 @@
 local M = {}
 
 M.default = {
-	style = "dark",
+  style = "dark",
 
-	italic_keywords = true,
+  italic_keywords = true,
 
-	italic_comments = true,
+  italic_comments = true,
 
-	italic_strings = true,
+  italic_strings = true,
 
-	diagnostic_text_highlight = true,
+  diagnostic_text_highlight = true,
 
-	on_highlights = function(hl, palette) end,
+  on_highlights = function(hl, palette) end,
 }
 
 M.setup = function(opts)
-	M.config = vim.tbl_deep_extend("keep", opts or {}, M.default)
+  M.config = vim.tbl_extend("keep", opts or {}, M.default)
 end
 
 M.load = function()
-	require("thorn.highlights").generate_highlights(M.config)
+  require("thorn.highlights").generate_highlights(M.config)
 end
 
 return M
