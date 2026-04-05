310e817c8cafd528fccffd7d5e8f606ce0547d44
jpwol <jpwolfe1981@gmail.com>
2026-04-05 19:46:44 -0400

    fix(blink): updated border highlight

--- lua/thorn/groups/blink.lua
+++ lua/thorn/groups/blink.lua
@@ -4,24 +4,24 @@ function M.get(c, opts)
 
   -- stylua: ignore
   local ret = {
-    BlinkCmpDoc                 = { fg = c.fg, bg = c.bg_float },
-    BlinkCmpDocBorder           = { fg = c.none, bg = c.bg_float },
+    BlinkCmpDoc                 = { fg = c.fg, bg         = c.bg_float },
+    BlinkCmpDocBorder           = { fg = c.lightgreen, bg = c.bg_float },
     BlinkCmpGhostText           = { fg = c.number },
-    BlinkCmpKindCodeium         = { fg = c.cyan, bg             = c.none },
-    BlinkCmpKindCopilot         = { fg = c.cyan, bg             = c.none },
-    BlinkCmpKindDefault         = { fg = c.fg, bg          = c.none },
-    BlinkCmpKindSupermaven      = { fg = c.cyan, bg             = c.none },
-    BlinkCmpKindTabNine         = { fg = c.cyan, bg             = c.none },
+    BlinkCmpKindCodeium         = { fg = c.cyan,   bg     = c.none },
+    BlinkCmpKindCopilot         = { fg = c.cyan,   bg     = c.none },
+    BlinkCmpKindDefault         = { fg = c.fg,     bg     = c.none },
+    BlinkCmpKindSupermaven      = { fg = c.cyan,   bg     = c.none },
+    BlinkCmpKindTabNine         = { fg = c.cyan,   bg     = c.none },
     BlinkCmpKind                = { fg = c.red },
-    BlinkCmpLabel               = { fg = c.fg, bg               = c.none },
+    BlinkCmpLabel               = { fg = c.fg,     bg     = c.none },
     BlinkCmpLabelDetail         = { fg = c.green },
     BlinkCmpLabelDescription    = { fg = c.green },
-    BlinkCmpLabelDeprecated     = { fg = c.number, bg        = c.none, strikethrough = true },
-    BlinkCmpLabelMatch          = { fg = c.cyan, bg            = c.none, bold = true },
-    BlinkCmpMenu                = { fg = c.green, bg               = c.bg_float },
-    BlinkCmpMenuBorder          = { fg = c.separator, bg = c.bg_float },
-    BlinkCmpSignatureHelp       = { fg = c.fg, bg               = c.bg_float },
-    BlinkCmpSignatureHelpBorder = { fg = c.green0, bg = c.bg_float },
+    BlinkCmpLabelDeprecated     = { fg = c.number, bg     = c.none, strikethrough = true },
+    BlinkCmpLabelMatch          = { fg = c.cyan,   bg     = c.none, bold = true },
+    BlinkCmpMenu                = { fg = c.green,  bg     = c.bg_float },
+    BlinkCmpMenuBorder          = { fg = c.lightgreen, bg = c.bg_float },
+    BlinkCmpSignatureHelp       = { fg = c.fg,     bg     = c.bg_float },
+    BlinkCmpSignatureHelpBorder = { fg = c.green0, bg     = c.bg_float },
   }
 
   require("thorn.groups.kinds").kinds(ret, "BlinkCmpKind%s")
